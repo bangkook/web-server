e793022e4075dae04d852e1452d0c944e06786bb
Mariam Hossam <m.hossam2551@gmail.com>
2023-11-30 22:23:57 +0200

    connection handler

--- server/main.c
+++ server/main.c
@@ -10,8 +10,11 @@
 #include <sys/wait.h>
 #include <signal.h>
 
+#include<request.h>
+
 #define PORT 8080
 #define BACKLOG 10
+#define BUFSIZE 1024
 
 void sigchld_handler(int s)
 {
@@ -19,7 +22,7 @@ void sigchld_handler(int s)
 }
 
 int main(int argc, char const* argv[]){
-    int server_fd, new_fd;
+    int server_fd, client_fd;
     struct sockaddr_in addr;
     struct sigaction sa;
     int sin_size = sizeof(struct sockaddr_in);
@@ -54,29 +57,63 @@ int main(int argc, char const* argv[]){
         perror("sigaction");
         exit(1);
     }
-    while(1){
-        if(listen(server_fd, BACKLOG) < 0) {
-            perror("listen failed");
-            exit(EXIT_FAILURE);
-        }
 
-        if((new_fd = accept(server_fd, (struct sockaddr *)&addr, &sin_size)) < 0) {
+    if(listen(server_fd, BACKLOG) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1) {
+
+        printf("Waiting for connections...\n");
+
+        if((client_fd = accept(server_fd, (struct sockaddr *)&addr, &sin_size)) < 0) {
             perror("accept failed");
             continue;
         }
 
-        if(!fork()) { // This is the child process
-            close(server_fd); // Child doesn't need the listener
-            if(send(new_fd, "Hello World!\n", 14, 0) < 0)
-                perror("send");
-            close(new_fd);
-            exit(EXIT_SUCCESS);
-        }
-        close(new_fd); // Parent does not need this
+        printf("Connected\n");
 
-        
+        handle_connection(client_fd);
     }
     
+    void handle_connection(int clntSocket) {
+        char buffer[BUFSIZE];
+
+        // Receive from client
+        size_t bytes_rcvd = recv(clntSocket, buffer, BUFSIZE, 0);
+        if(bytes_rcvd < 0) {
+            perror("recieve failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Parse the HTTP request
+        struct Request req = parse_request(buffer);
+
+        // Print the request
+        printf("REQUEST:\n%s %s %s\n%s\n", req->method, req->url, req->version, req->options);
+        fflush(stdout);
+
+        // Read file and sed its contents to the client
+        FILE *fp = fopen(req->url, "r");
+        if(fp == NULL) {
+            printf("ERROR(open): %s\n", req->url);
+            close(clntSocket);
+            return;
+        }
+
+        char* header = "HTTP/1.1 200 OK \r\n";
+        size_t bytes_read;
+        while((bytes_read = fread(buffuer, 1, BUFSIZE, fp)) > 0) {
+            printf("Sending %2u bytes\n", bytes_read);
+            write(clntSocket, buffer, bytes_read);
+        } 
+
+        close(clntSocket);
+        fclose(fp);
+        printf("Connection closed\n");
+
+    }
     
 
     return 0;
